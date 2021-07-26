ed724e451f7fbe6a83dd685f478b5508b9563ada
Mihai <veres85@yahoo.com>
2021-07-26 13:05:20 +0000

    removed stories of tokens, we're only keeping docs

--- space/stories/space.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { theme } from '~/all';
-import { SystemUIShowcases } from '@divriots/dockit-react/systemui-showcases';
-import { StoryLayout } from '~/layout';
-export default StoryLayout;
-
-export const space = () => <SystemUIShowcases theme={theme} showcaseKey="space" />;
